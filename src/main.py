47ef995cc9847bc6c8ad95da30c64113790be27f
sawertynn <sawertynn@gmail.com>
2025-02-26 13:30:33 +0100

    moved source files to src folder

--- src/main.py
+++ src/main.py
@@ -10,6 +10,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         screen.fill(0x000)
         pygame.display.flip()
@@ -17,6 +20,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        dms = clock.tick(FRAMERATE)
+        dt = dms / 1000
 
 
 if __name__ == "__main__":
